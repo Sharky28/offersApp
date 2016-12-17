ee4399bd331ea32e88fdb18e8f45f5f392c09168
Sharmarke <sharmarke.ugas@gmail.com>
2016-12-17 12:55:16 +0000

    Configuring JNDI Data Source

--- spring-50DEC/src/com/sharky/spring/web/controllers/OffersController.java
+++ spring-50DEC/src/com/sharky/spring/web/controllers/OffersController.java
@@ -1,17 +1,26 @@
 package com.sharky.spring.web.controllers;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OffersController {
 
 	@RequestMapping("/")
-	public String showHome(HttpSession session) {
-		session.setAttribute("name", "borris");
-		return "home";
+	public ModelAndView showHome() {
+		// session.setAttribute("name", "Sharky");
+		ModelAndView mView = new ModelAndView("home");
+
+		Map<String, Object> model = mView.getModel();
+
+		model.put("name", "River");
+
+		return mView;
 
 	}
 }
