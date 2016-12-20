e4b12e4479b4240491a34d7bd1d36360679521ab
Sharmarke <sharmarke.ugas@gmail.com>
2016-12-20 12:51:44 +0000

    added login form , security exceptions/controller and related css

---
+++ spring-50DEC/src/com/sharky/spring/web/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.sharky.spring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/login")
+	private String showLogin() {
+		return "login";
+	}
+
+}
