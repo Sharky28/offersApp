ab16614181e86fd896cbfade51a29796ff2d4aff
Sharmarke <sharmarke.ugas@gmail.com>
2016-12-24 13:32:24 +0000

    added logging

--- spring-50DEC/src/com/sharky/spring/web/controllers/HomeController.java
+++ spring-50DEC/src/com/sharky/spring/web/controllers/HomeController.java
@@ -1,16 +1,16 @@
 package com.sharky.spring.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+private static Logger logger = Logger.getLogger(HomeController.class);
 	@RequestMapping("/")
 	public String showHome() {
-
+		logger.info("showing home page...");
 		return "home";
-
 	}
 
 	
